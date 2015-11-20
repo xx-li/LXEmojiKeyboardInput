e6f72f846b893fdff5f3c23f5eadc8d6475d701a
李新星 <lixinxing@lixinxingdeiMac.local>
2015-11-20 13:50:16 +0800

    update cocoapods file

--- LXEmojiKeyboardInputDemo/LXEmojiKeyboard/ViewController.m
+++ LXEmojiKeyboardInputDemo/LXEmojiKeyboard/ViewController.m
@@ -94,7 +94,6 @@
     
     [inputView.textView resignFirstResponder];
 
-    
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(durtion * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         if (type == LXKeyboardTypeDefault) {
             _emojiView.textView = nil;
